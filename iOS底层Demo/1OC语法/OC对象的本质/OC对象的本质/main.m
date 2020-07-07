50d5673873a8b7b9a6dc97a98a4bcbcfeb8f06eb
jonzhang <jon.zhang@trustlife.com>
2020-07-07 12:02:26 +0800

    oc对象本质

---
+++ iOS底层Demo/1OC语法/OC对象的本质/OC对象的本质/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  OC对象的本质
+//
+//  Created by jonzhang on 2020/7/7.
+//  Copyright © 2020 xinmei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSObject * obj = [[NSObject alloc] init];
+        
+        
+
+
+    }
+    return 0;
+}
---
+++ iOS底层Demo/1OC语法/OC对象的本质自定义类的内部实现/OC对象的本质/main.m
@@ -0,0 +1,71 @@
+//
+//  main.m
+//  OC对象的本质
+//
+//  Created by jonzhang on 2020/7/7.
+//  Copyright © 2020 xinmei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+struct Student_IMPL {
+    
+    Class isa ;
+    
+    int _no;
+    int _age;
+    
+};
+
+
+@interface Student : NSObject{
+    
+    
+  @public
+    int _no;
+    int _age;
+    
+}
+
+
+@end
+@implementation Student
+
+
+
+@end
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        
+       
+        Student *stu = [[Student alloc] init];
+        
+        stu->_no = 23;
+        stu->_age = 24;
+        
+        NSLog(@"---%@",stu);
+        
+        
+        // 强行转化成 结构体
+        struct Student_IMPL * stuImpl = (__bridge struct Student_IMPL *)(stu) ;
+        
+        NSLog(@"_no = %d     _age = %d",stuImpl -> _no,stuImpl -> _age);
+        
+        
+        
+        NSLog(@"%zd-----%zd",class_getInstanceSize([NSObject class]),class_getInstanceSize([Student class]));
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+    }
+    return 0;
+}
